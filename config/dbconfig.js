90acc84f2ee631cd4b2d63d7e1ac88c1ae9995de
unknown <ifexcojerry4ril@gmail.com>
2021-05-06 05:53:10 -0700

    version 1

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,8 @@
-require("dotenv/config");
+// require("dotenv/config");
 
 module.exports = {
+  // secret: process.env.TOKEN_SECRET,
+  // database: process.env.DATABASE_URL,
   secret: "hfksecret",
   database:
     "mongodb+srv://hfkadmin:hfkpassword@cluster0.ppoi5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
