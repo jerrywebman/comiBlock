ef9d0d7bc59da294b401dd9a83d20a43e56d5e22
unknown <ifexcojerry4ril@gmail.com>
2021-06-23 08:11:49 -0700

    cors config next

--- server.js
+++ server.js
@@ -17,6 +17,31 @@ if (process.env.NODE_ENV === "development") {
 
 ///routes
 //cors allows us to call data/api from cross domains
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(cors({ origin: "https://localhost:3000", credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
